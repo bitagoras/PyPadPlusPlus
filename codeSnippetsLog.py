cb7b92769b68d8444fce90cc5e8d483da9f36658
Bitagoras <christian.schirm@gmail.com>
2018-11-18 11:38:45 +0100

    remove \r in line breaks of codeSnippetsLog.py

--- codeSnippetsLog.py
+++ codeSnippetsLog.py
@@ -8,7 +8,7 @@ path = os.path.join(logDir, filename)
 
 if text:
     with file(path,'a') as f:
-        f.write('#%% ' + '_'*25 + ' ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' + '_'*25 + '\n\n' + text.strip() + '\n\n')
+        f.write('#%% ' + '_'*25 + ' ' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' + '_'*25 + '\n\n' + text.replace('\r','').strip() + '\n\n')
     n = len(text.splitlines())
     Npp.notepad.messageBox('%i lines logged to %s'%(n,filename))
 else:
