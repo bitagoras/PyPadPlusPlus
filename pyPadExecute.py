f0383e3dced8bef0a4e88aafe91e976974dc82f0
bitagoras <bitagoras@users.noreply.github.com>
2018-11-20 08:34:29 +0100

    Update pyPadExecute.py

--- pyPadExecute.py
+++ pyPadExecute.py
@@ -2,7 +2,7 @@
 # PyPadPlusPlus: Execute command
 #
 # Put a shortcut on this script to start the interactive programming
-# Recommended is <Ctrl> + <Enter>
+# Recommended is <Shift> + <Enter>
 
 try: Npp.pypad != None
 except: from pyPadStart import *
