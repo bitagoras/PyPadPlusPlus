e708e7e4072ff24b3350d47c55f41602a3a526a8
bitagoras <bitagoras@users.noreply.github.com>
2018-11-20 08:35:21 +0100

    Update pyPadExecuteFix.py

--- pyPadExecuteFix.py
+++ pyPadExecuteFix.py
@@ -2,7 +2,7 @@
 # PyPadPlusPlus: Execute command while cursor stays at position
 #
 # Put a shortcut on this script to start the interactive programming
-# Recommended is <Shift> + <Ctrl> + <Enter>
+# Recommended is <Ctrl> + <Enter>
 
 try: Npp.pypad != None
 except: from pyPadStart import *
